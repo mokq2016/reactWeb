4e665a2b9b780d39744f3f2ee0519bf2df71834e
mkq <873302396@qq.com>
2018-08-16 23:22:25 +0800

    uodate

--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -1,23 +1,73 @@
 import React, { Component } from "react";
 import { Icon } from "antd";
 import cx from "classnames";
+
+import CSSAnimate from "../cssAnimate/index";
 import "./style.less";
 
 export default class Panel extends Component {
   static defaultProps = {
     prefix: "antui-panel"
   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: props.collapse || false,
+      expand: props.expand || false,
+      refresh: 0,
+      animationName: ""
+    };
+  }
+  onCollapse = collapse => e => {
+    const { onChange } = this.props;
+
+    this.setState({
+      collapse,
+      expand: false
+    });
+
+    if (onChange) {
+      onChange({
+        collapse,
+        expand: false
+      });
+    }
+  };
+
+  onRefresh = () => {
+    this.setState({
+      refresh: this.state.refresh + 1,
+      animationName: "fadeIn"
+    });
+    this.props.onRefresh && this.props.onRefresh();
+  };
 
   render() {
-    const { prefix, children, header, title } = this.props;
-    const classnames = cx(prefix);
+    const { expand, collapse, refresh, animationName } = this.state;
+    const { prefix, className, children, header, title } = this.props;
+    const classnames = cx(prefix, className, {
+      "panel-fullscreen": !!expand,
+      "panel-collapsed": !!collapse
+    });
     const Header =
       typeof header === "undefined" ? (
         <div className={`${prefix}-header`}>
           <span className={`${prefix}-header-title`}>{title}</span>
           <span className={`${prefix}-header-controls`}>
-            <a className="panel-control-loader">
+            <a className="panel-control-loader" onClick={this.onRefresh}>
               <Icon type="reload" />
+            </a>
+            <a className="panel-control-loader">
+              <Icon type="arrows-alt" />
+            </a>
+            <a
+              className="panel-control-loader"
+              onClick={this.onCollapse(collapse ? false : true)}
+            >
+              <Icon type={`${collapse ? "plus" : "minus"}`} />
+            </a>
+            <a className="panel-control-loader">
+              <Icon type="close" />
             </a>
           </span>
         </div>
@@ -27,7 +77,16 @@ export default class Panel extends Component {
     return (
       <div className={classnames}>
         {Header}
-        <div className={`${prefix}-body`}>{children}</div>
+        <div className={`${prefix}-body`}>
+          <CSSAnimate
+            className="panel-content"
+            type={animationName}
+            callback={e => this.setState({ animationName: "" })}
+            key={refresh}
+          >
+            {children}
+          </CSSAnimate>
+        </div>
       </div>
     );
   }
